07e75270e3b576b6b53ade91b59297f052ea0039
Arthur Bouie <arthurbouie@gmail.com>
2014-05-07 23:36:06 -0400

    OY!

--- app/objects.js
+++ app/objects.js
@@ -3,15 +3,26 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      fn = "Yo, ";
+      obj = "Rebecca!";
+      return fn + obj;
     },
 
     alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = 'What\'s up';
     },
 
     iterate : function(obj) {
+    
+  var ret = [];
 
+      for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+          ret.push(prop + ': ' + obj[prop]);
+        }
+      }
+
+      return ret;
     }
   };
 });
